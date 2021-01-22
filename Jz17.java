eba0457c233b5ffad62064ca52dc9daac5dd5f6e
Kuonji-Alice <fouriertransform@126.com>
2021-01-22 21:26:22 +0800

    jz17

---
+++ Jz17.java
@@ -0,0 +1,27 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Jz17 {
+    public ListNode FindKthToTail(ListNode head,int k) {
+        if(head == null) return null;
+        ListNode list = head;
+        int count = 0;
+        while(list != null){
+            count++;
+            list = list.next;
+        }
+        if(k > count) return null;
+        count -= k;
+        while(count >= 1){
+            head = head.next;
+            count--;
+        }
+        return head;
+    }
+}
