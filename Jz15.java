768bb60ef354f9a42cc43f7d3548841f7d1abecd
Kuonji-Alice <fouriertransform@126.com>
2021-01-22 20:22:53 +0800

    Jz15.java

---
+++ Jz15.java
@@ -0,0 +1,14 @@
+public class Jz15 {
+    public double Power(double base, int exponent) {
+        if(base == 0) return 0;
+        if(exponent == 0) return 1;
+        double sum = 1;
+        for(int i = 0; i < Math.abs(exponent); i++){
+            sum *= base;
+        }
+        if(exponent < 0){
+            sum = 1/sum;
+        }
+        return sum;
+  }
+}
