0a971f5d54a8ab002bf7ef156d78fd11d8223a05
Kuonji-Alice <fouriertransform@126.com>
2021-01-25 14:16:13 +0800

    BigInteger version jz07

--- Jz07.java
+++ Jz07.java
@@ -1,6 +1,13 @@
-public class Jz07 {
-    public int Fibonacci(int n) {
+import java.math.BigInteger;
+class Jz07 {
+    public int fib(int n) {
         if(n == 0 || n == 1) return n;
-        return Fibonacci(n - 1) + Fibonacci(n - 2);
+        ArrayList<BigInteger> fibArr = new ArrayList<>();
+        fibArr.add(new BigInteger("0"));
+        fibArr.add(new BigInteger("1"));
+        for(int i = 2; i <= n; i++){
+            fibArr.add(fibArr.get(i - 1).add(fibArr.get(i - 2)));
+        }
+        return fibArr.get(fibArr.size() - 1).divideAndRemainder(new BigInteger("1000000007"))[1].intValue();
     }
 }
