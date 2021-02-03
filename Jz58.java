67366b1c636c1c955d59c5076eae8851f5661010
Kuonji-Alice <fouriertransform@126.com>
2021-02-03 19:55:44 +0800

    Jz58

---
+++ Jz58.java
@@ -0,0 +1,32 @@
+/*
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    
+    boolean ret = true;
+    boolean isSymmetrical(TreeNode pRoot)
+    {
+        if(pRoot == null) return true;
+        return rec(pRoot.left, pRoot.right);
+    }
+    
+    boolean rec(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if((left == null && right != null)
+          || (left != null && right == null) || left.val != right.val){
+            return false;
+        }
+        return rec(left.left, right.right);
+        
+    }
+}
