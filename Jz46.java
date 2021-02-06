230e4d1430dfe0a30de30d23abc6e9b29c2d7029
Kuonji-Alice <fouriertransform@126.com>
2021-02-06 16:50:01 +0800

    new solution

--- Jz46.java
+++ Jz46.java
@@ -1,27 +1,12 @@
-public class Jz46 {
+public class Solution {
     public int LastRemaining_Solution(int n, int m) {
         if(n == 0 || m == 0) return -1;
-        ListNode head = new ListNode(0);
-        ListNode temp = head;
-        for(int i = 1; i < n; i++){
-            temp.next = new ListNode(i);
-            temp = temp.next;
-        }
-        temp.next = head;
-        temp = head;
-        while(temp.next != temp){
-            for(int i = 0; i < m - 2; i++){
-                temp = temp.next;
-            }
-            temp.next = temp.next.next;
-            temp = temp.next;
-        }
-        return temp.val;
+        return rec(n,m);
     }
-}
-
-class ListNode{
-    int val;
-    ListNode next;
-    ListNode(int x){val = x;}
+    
+    public int rec(int n, int m){
+        if(n == 1) return 0;
+        int x = rec(n - 1, m);
+        return (m%n + x)%n;
+    }
 }
